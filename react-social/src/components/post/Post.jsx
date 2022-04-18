13df8136764f4e8ce85c54eda260c1f63284c89f
Dlawlet <Daremol@protonmail.com>
2022-04-18 20:16:47 +0200

    post's stars responsive

--- react-social/src/components/post/Post.jsx
+++ react-social/src/components/post/Post.jsx
@@ -1,6 +1,7 @@
 import "./post.css";
 import { format } from "timeago.js";
 import { Link } from "react-router-dom";
+import { Rating } from 'react-simple-star-rating';
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import axios from "axios";
@@ -8,7 +9,9 @@ import axios from "axios";
 export default function Post(post) {
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
   const [user, setUser] = useState({});
-  const { user: currentUser } = useContext(AuthContext);
+  const sw = window.innerWidth<890 
+  ? 1
+  : 2;
   useEffect(() => {
     const fetchUser = async () => {
       const res = await axios.get(`/users?userId=${post.post.userGvId}`);
@@ -16,8 +19,11 @@ export default function Post(post) {
     };
     fetchUser();
   }, [post.post.userGvId]);
+  
+  
 
   return (
+    
     <div className="post">
       <div className="postWrapper">
         <div className="postTop">
@@ -38,6 +44,17 @@ export default function Post(post) {
           </div>
         </div>
         <div className="postCenter">
+          <Rating className="ratedisplay" ratingValue={post.rate} /* Available Props */ 
+                        fillColorArray={['#EBE007', '#F2F20C', '#D0D40B', '#B7DB00', '#ABF513']} 
+                        readonly={true}
+                        initialValue={post.post.rate}
+                        fillColor={"#B7DB00"}
+                        allowHalfIcon={true}
+                        transition={true}
+                        size={21*sw}
+                        showTooltip
+                        tooltipStyle={{fontSize:"80%"}}
+                />
           <span className="postText">{post.post?.review}</span>
         </div>
       </div>
