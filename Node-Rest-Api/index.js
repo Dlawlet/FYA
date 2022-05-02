14b842939d0f78f1b6522e2f8d2105d340aa43c7
Dlawlet <Daremol@protonmail.com>
2022-05-02 10:07:43 +0200

    correction connecction backend through node indexjs

--- Node-Rest-Api/index.js
+++ Node-Rest-Api/index.js
@@ -1,54 +1,56 @@
-const express = require('express');
-const app = express();
-const mongoose = require("mongoose");
+const express = require('express')
+const app = express()
+const mongoose = require("mongoose")
 const dotenv = require("dotenv");
-const helmet = require("helmet");
-const morgan = require("morgan");
-const multer = require("multer");
-const userRoute = require("./routes/users");
-const authRoute = require("./routes/auth");
-const postRoute = require("./routes/posts");
-const path = require("path");
-const cors = require("cors");
+const helmet = require("helmet")
+const morgan = require("morgan")
+const multer = require("multer")
+const userRoute = require("./routes/users")
+const authRoute = require("./routes/auth")
+const postRoute = require("./routes/posts")
+const cors = require("cors")
 
 dotenv.config();
- 
-mongoose.connect(
-  process.env.MONGO_URL,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  () => {
-    console.log("Connected to MongoDB");
-  }
-);
-app.use("/images", express.static(path.join(__dirname, "public/images")));
-app(cors());
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connection Successfull"))
+  .catch((err) => {
+    console.error(err)
+  })
+
+app.use(
+  cors()
+)
 //middleware
-app.use(express.json());
-app.use(helmet());
-app.use(morgan("common"));
+app.use(express.json())
+app.use(helmet())
+app.use(morgan("common"))
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "public/images/person");
+    cb(null, "public/images/person")
   },
   filename: (req, file, cb) => {
-    cb(null, req.body.name);
+    cb(null, req.body.name)
   },
-});
+})
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage: storage })
 app.post("/api/upload", upload.single("file"), (req, res) => {
   try {
-    return res.status(200).json("File uploded successfully");
+    return res.status(200).json("File uploded successfully")
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
-});
+})
 
-app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
-app.use("/api/posts", postRoute);
+app.use("/api/auth", authRoute)
+app.use("/api/users", userRoute)
+app.use("/api/posts", postRoute)
 
-app.listen(8800, () => {
-console.log("Backend server is running!");
-});
+app.listen(8080, () => {
+console.log("Backend server is running!")
+})
